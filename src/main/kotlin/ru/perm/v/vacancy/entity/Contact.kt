0063938fd97cd9f688fe032c6668a0c28eabd900
Черепахин Василий <vasi.che@gmail.com>
2024-05-18 22:34:03 +0500

    add Contact.kt

---
+++ src/main/kotlin/ru/perm/v/vacancy/entity/Contact.kt
@@ -0,0 +1,55 @@
+package ru.perm.v.vacancy.entity
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "company")
+class Contact {
+    @Id
+    @Column(name = "n", nullable = false)
+    open var n: Long = -1L
+    @Column(name = "name", nullable = false)
+    open var name: String = ""
+    @Column(name = "email", nullable = false)
+    open var email: String = ""
+    @Column(name = "phone", nullable = false)
+    open var phone: String = ""
+    @Column(name = "comment", nullable = false)
+    open var comment: String = ""
+
+    constructor() {
+    }
+
+    constructor(n: Long, name: String, email: String, phone: String, comment: String) {
+        this.n = n
+        this.name = name
+        this.email = email
+        this.phone = phone
+        this.comment = comment
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Contact) return false
+
+        if (n != other.n) return false
+        if (name != other.name) return false
+        if (email != other.email) return false
+        if (phone != other.phone) return false
+        if (comment != other.comment) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = n.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + email.hashCode()
+        result = 31 * result + phone.hashCode()
+        result = 31 * result + comment.hashCode()
+        return result
+    }
+}
---
+++ src/test/kotlin/ru/perm/v/vacancy/entity/ContactTest.kt
@@ -0,0 +1,31 @@
+package ru.perm.v.vacancy.entity
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ContactTest {
+    @Test
+    fun defaultConstructor() {
+        val c = Contact()
+
+        assertNotNull(c)
+        assertEquals(-1L, c.n)
+    }
+    @Test
+    fun secondaryConstructor() {
+        val N = 100L
+        val NAME = "name"
+        val EMAIL = "EMAIL"
+        val PHONE = "PHONE"
+        val COMMENT = "COMMENT"
+
+        val contact = Contact(N, NAME, EMAIL, PHONE, COMMENT)
+
+        assertNotNull(contact)
+        assertEquals(N, contact.n)
+        assertEquals(NAME, contact.name)
+        assertEquals(EMAIL, contact.email)
+        assertEquals(PHONE, contact.phone)
+        assertEquals(COMMENT, contact.comment)
+    }
+}
