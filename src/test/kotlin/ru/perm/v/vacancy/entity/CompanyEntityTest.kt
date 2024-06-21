9a3b447352caf11878eb4e5566eb09a54766f6ea
Черепахин Василий <vasi.che@gmail.com>
2024-06-21 11:15:42 +0500

    add CompanyEntityTest.kt

--- src/test/kotlin/ru/perm/v/vacancy/entity/CompanyEntityTest.kt
+++ src/test/kotlin/ru/perm/v/vacancy/entity/CompanyEntityTest.kt
@@ -11,4 +11,46 @@ class CompanyEntityTest {
         assertEquals("", company.name)
         assertEquals(-1L, company.n)
     }
+
+    @Test
+    fun constructorWithName() {
+        val company = CompanyEntity("test")
+
+        assertNotNull(company)
+        assertEquals("test", company.name)
+        assertEquals(-1L, company.n)
+    }
+
+    @Test
+    fun constructorWithIdAndName() {
+        val company = CompanyEntity(100L, "test")
+
+        assertNotNull(company)
+        assertEquals("test", company.name)
+        assertEquals(100L, company.n)
+    }
+
+    @Test
+    fun equalsTest() {
+        val company1 = CompanyEntity(100L, "test")
+        val company2 = CompanyEntity(100L, "test")
+
+        assertEquals(company1, company2)
+    }
+
+    @Test
+    fun notEqualsByNameTest() {
+        val company1 = CompanyEntity(100L, "-")
+        val company2 = CompanyEntity(100L, "test")
+
+        assertNotEquals(company1, company2)
+    }
+
+    @Test
+    fun notEqualsByIdTest() {
+        val company1 = CompanyEntity(1L, "test")
+        val company2 = CompanyEntity(100L, "test")
+
+        assertNotEquals(company1, company2)
+    }
 }
