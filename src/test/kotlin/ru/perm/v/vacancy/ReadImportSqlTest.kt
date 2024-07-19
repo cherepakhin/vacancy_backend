a63e4260f19f9d2a9355c455f36f82c8f8fd64e9
Черепахин Василий <vasi.che@gmail.com>
2024-07-19 12:20:10 +0500

    add ReadImportSqlTest

---
+++ src/test/kotlin/ru/perm/v/vacancy/ReadImportSqlTest.kt
@@ -0,0 +1,40 @@
+package ru.perm.v.vacancy
+
+import org.junit.jupiter.api.Test
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import kotlin.test.assertTrue
+
+
+class ReadImportSqlTest {
+    @Test
+    fun read() {
+        val inputStream: InputStream = this.javaClass.getResourceAsStream("/import.sql")
+        val fileContent: String = readFromInputStream(inputStream)
+
+        assertTrue(fileContent.length > 0)
+    }
+
+    @Test
+    fun testForKotlin() {
+        val fileContent = this::class.java.getResource("/import.sql").readText()
+
+        assertTrue(fileContent.length > 0)
+    }
+
+    @Throws(IOException::class)
+    private fun readFromInputStream(inputStream: InputStream): String {
+        val resultStringBuilder = StringBuilder()
+        BufferedReader(InputStreamReader(inputStream)).use {
+                br,
+            ->
+            var line: String?
+            while ((br.readLine().also { line = it }) != null) {
+                resultStringBuilder.append(line).append("\n")
+            }
+        }
+        return resultStringBuilder.toString()
+    }
+}
