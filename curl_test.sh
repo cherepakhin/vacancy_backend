a2a5bbe20ee6fd6f30760a88cd2a80a122eb501e
Черепахин Василий <vasi.che@gmail.com>
2024-10-05 21:26:20 +0500

    add curl_test.sh

--- curl_test.sh
+++ curl_test.sh
@@ -1,29 +1,42 @@
-// find companies by N AND name in  request jsom
-curl --header "Content-Type: application/json" --request PUT --data '{"n": 1,  "name": "COMPANY_1"}' 'http://127.0.0.1:8980/vacancy/api/company/find/'
-// answer: [{"n":1,"name":"COMPANY_1"}]
+# find companies by N AND name in  request jsom
+curl --header "Content-Type: application/json" --request PUT --data '{"n": 1,  "name": "COMPANY_1"}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
+# / answer: [{"n":1,"name":"COMPANY_1"}]
+echo -e "\n"
 
-// find companies by LIKE name
-curl --header "Content-Type: application/json" --request PUT --data '{"name": "COMPANY"}' 'http://127.0.0.1:8980/vacancy/api/company/find/'
-// answer: [{"n":1,"name":"COMPANY_1"},{"n":2,"name":"COMPANY_2"},{"n":3,"name":"3_COMPANY"}]
+# find companies by LIKE name
+curl --header "Content-Type: application/json" --request PUT --data '{"name": "COMPANY"}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
+# answer: [{"n":1,"name":"COMPANY_1"},{"n":2,"name":"COMPANY_2"},{"n":3,"name":"3_COMPANY"}]
+echo -e "\n"
 
-// find companies by EQUAL name
-curl --header "Content-Type: application/json" --request PUT --data '{"name": "COMPANY_2"}' 'http://127.0.0.1:8980/vacancy/api/company/find/'
-// answer:  [{"n":2,"name":"COMPANY_2"}]
+# find companies by EQUAL name
+curl --header "Content-Type: application/json" --request PUT --data '{"name": "COMPANY_2"}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
+# answer:  [{"n":2,"name":"COMPANY_2"}]
+echo -e "\n"
 
-// find companies by LIKE name
-curl --header "Content-Type: application/json" --request PUT --data '{"name": "2"}' 'http://127.0.0.1:8980/vacancy/api/company/find/'
-//answer: [{"n":2,"name":"COMPANY_2"}]
+# find companies by LIKE name
+curl --header "Content-Type: application/json" --request PUT --data '{"name": "2"}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
+# answer: [{"n":2,"name":"COMPANY_2"}]
+echo -e "\n"
 
-// find companies by LIKE name
-curl --header "Content-Type: application/json" --request PUT --data '{"name": "COMPANY"}' 'http://127.0.0.1:8980/vacancy/api/company/find/'
-//answer: [{"n":1,"name":"COMPANY_1"},{"n":2,"name":"COMPANY_2"},{"n":3,"name":"3_COMPANY"}]
+# find companies by LIKE name
+curl --header "Content-Type: application/json" --request PUT --data '{"name": "COMPANY"}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
+# answer: [{"n":1,"name":"COMPANY_1"},{"n":2,"name":"COMPANY_2"},{"n":3,"name":"3_COMPANY"}]
+echo -e "\n"
 
-// find company by N
-curl --header "Content-Type: application/json" --request PUT --data '{"n" : 1}' 'http://127.0.0.1:8980/vacancy/api/company/find/'
-[{"n":1,"name":"COMPANY_1"}
+# find company by N
+curl --header "Content-Type: application/json" --request PUT --data '{"n" : 1}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
+# [{"n":1,"name":"COMPANY_1"}
+echo -e "\n"
 
-// get ALL companies sorted by column n
-curl --request GET  'http://127.0.0.1:8980/vacancy/api/company/sortByColumn/n'
+# get ALL companies sorted by column n
+echo 'http://127.0.0.1:8980/vacancy/api/company/sortByColumn/n'
+curl --request GET  'http://127.0.0.1:8980/vacancy/api/company/sortByColumn/n' | jq
+echo -e "\n"
 
-// get ALL companies sorted by column name
-curl --request GET  'http://127.0.0.1:8980/vacancy/api/company/sortByColumn/name'
+# get ALL companies sorted by column name
+echo 'GET http://127.0.0.1:8980/vacancy/api/company/sortByColumn/name'
+curl --request GET 'http://127.0.0.1:8980/vacancy/api/company/sortByColumn/name' | jq
+
+## VERBOSE
+#curl -v GET  'http://127.0.0.1:8980/vacancy/api/company/sortByColumn/name' | jq
+echo -e "\n"
