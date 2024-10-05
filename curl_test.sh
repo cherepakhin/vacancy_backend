eac3ba21ae49392cd7cc8b60dfeec421e093a23d
Черепахин Василий <vasi.che@gmail.com>
2024-10-05 21:36:35 +0500

    format add curl_test.sh

--- curl_test.sh
+++ curl_test.sh
@@ -1,39 +1,40 @@
-# find companies by N AND name in  request jsom
+echo 'find companies by request json'
 curl --header "Content-Type: application/json" --request PUT --data '{"n": 1,  "name": "COMPANY_1"}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
 # / answer: [{"n":1,"name":"COMPANY_1"}]
 echo -e "\n"
 
-# find companies by LIKE name
+echo 'find companies by name LIKE COMPANY'
 curl --header "Content-Type: application/json" --request PUT --data '{"name": "COMPANY"}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
 # answer: [{"n":1,"name":"COMPANY_1"},{"n":2,"name":"COMPANY_2"},{"n":3,"name":"3_COMPANY"}]
 echo -e "\n"
 
-# find companies by EQUAL name
+echo 'find companies by name EQUAL COMPANY_2'
 curl --header "Content-Type: application/json" --request PUT --data '{"name": "COMPANY_2"}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
 # answer:  [{"n":2,"name":"COMPANY_2"}]
 echo -e "\n"
 
-# find companies by LIKE name
+echo 'find companies by name LIKE %2%'
 curl --header "Content-Type: application/json" --request PUT --data '{"name": "2"}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
 # answer: [{"n":2,"name":"COMPANY_2"}]
 echo -e "\n"
 
-# find companies by LIKE name
+echo 'find companies by name LIKE COMPANY'
+echo 'PUT http://127.0.0.1:8980/vacancy/api/company/find/'
 curl --header "Content-Type: application/json" --request PUT --data '{"name": "COMPANY"}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
 # answer: [{"n":1,"name":"COMPANY_1"},{"n":2,"name":"COMPANY_2"},{"n":3,"name":"3_COMPANY"}]
 echo -e "\n"
 
-# find company by N
+echo 'find company by N'
 curl --header "Content-Type: application/json" --request PUT --data '{"n" : 1}' 'http://127.0.0.1:8980/vacancy/api/company/find/' | jq
 # [{"n":1,"name":"COMPANY_1"}
 echo -e "\n"
 
-# get ALL companies sorted by column n
+echo 'get ALL companies sorted by column n'
 echo 'http://127.0.0.1:8980/vacancy/api/company/sortByColumn/n'
-curl --request GET  'http://127.0.0.1:8980/vacancy/api/company/sortByColumn/n' | jq
+curl --request GET 'http://127.0.0.1:8980/vacancy/api/company/sortByColumn/n' | jq
 echo -e "\n"
 
-# get ALL companies sorted by column name
+echo 'get ALL companies sorted by column name'
 echo 'GET http://127.0.0.1:8980/vacancy/api/company/sortByColumn/name'
 curl --request GET 'http://127.0.0.1:8980/vacancy/api/company/sortByColumn/name' | jq
 echo -e "\n"
